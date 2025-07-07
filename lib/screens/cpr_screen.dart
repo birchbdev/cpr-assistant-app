8596754ed86bef9e2f620599246b330898991029
Bryan Birch <birchb.dev@gmail.com>
2025-07-07 13:31:50 -0700

    feature: added audio and visual cues with metronome

--- lib/screens/cpr_screen.dart
+++ lib/screens/cpr_screen.dart
@@ -1,7 +1,74 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-class CPRScreen extends StatelessWidget {
+import 'package:audioplayers/audioplayers.dart';
+
+final AudioPlayer _audioPlayer = AudioPlayer();
+bool _pulseOn = false;
+late Timer _metronomeTimer;
+
+class CPRScreen extends StatefulWidget {
   const CPRScreen({super.key});
+
+  @override
+  State<CPRScreen> createState() => _CPRScreenState();
+}
+
+class _CPRScreenState extends State<CPRScreen> {
+  late Timer _timer;
+  int _elapsedSeconds = 0;
+  bool _audioEnabled = true;
+  bool _visualEnabled = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _elapsedSeconds++;
+      });
+    });
+    // Metronome: ~110 bpm (every 545ms)
+    _metronomeTimer = Timer.periodic(const Duration(milliseconds: 545), (timer) {
+      _playTick();
+      if (_visualEnabled) {
+        setState(() {
+          _pulseOn = true;
+        });
+        Future.delayed(const Duration(milliseconds: 100), () {
+          if (mounted) {
+            setState(() => _pulseOn = false);
+          }
+        });
+      }
+    //   setState(() {
+    //     _pulseOn = !_pulseOn; // Toggle visual cue
+    //   });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+    _metronomeTimer.cancel();
+    _audioPlayer.dispose();
+  }
+
+  void _playTick() async {
+    // await _audioPlayer.play(AssetSource('sounds/tick.wav'));
+    if (_audioEnabled) {
+      await _audioPlayer.play(AssetSource('sounds/tick.wav'));
+    }
+  }
+
+  String _formatElapsedTime() {
+    final minutes = (_elapsedSeconds ~/ 60).toString().padLeft(2, '0');
+    final seconds = (_elapsedSeconds % 60).toString().padLeft(2, '0');
+    return '$minutes:$seconds';
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -17,18 +84,57 @@ class CPRScreen extends StatelessWidget {
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 30),
-            const Text('Elapsed Time: 00:00'),
+            Text(
+              'Elapsed Time: ${_formatElapsedTime()}',
+              style: const TextStyle(fontSize: 20),
+            ),
             const SizedBox(height: 30),
-            Container(
+
+
+              Row( // does this go here?
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      setState(() => _audioEnabled = !_audioEnabled);
+                    },
+                    icon: Icon(
+                      _audioEnabled ? Icons.volume_up : Icons.volume_off,
+                      color: Colors.white,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      setState(() => _visualEnabled = !_visualEnabled);
+                    },
+                    icon: Icon(
+                      _visualEnabled ? Icons.visibility : Icons.visibility_off,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+
+
+            AnimatedContainer(
               width: double.infinity,
               height: 150,
-              color: Colors.grey.shade800,
+              duration: const Duration(milliseconds: 100),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                border: _visualEnabled && _pulseOn
+                    ? Border.all(color: Colors.white, width: 4)
+                    : Border.all(color: Colors.transparent),
+              ),
               alignment: Alignment.center,
               child: const Text(
-                'Waveform Placeholder',
-                style: TextStyle(color: Colors.white70),
+                'Compression Rhythm',
+                style: TextStyle(color: Colors.white70, fontSize: 18),
               ),
             ),
+
+
+
             const SizedBox(height: 30),
             ElevatedButton(
               onPressed: () {
